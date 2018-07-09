10ac01c3a7efbc06e7095cb29061d4a2a838afc1
fuyangyang <fuyangyang@meituan.com>
2018-07-09 14:37:16 +0800

    thread

---
+++ thread/threadtest.cpp
@@ -0,0 +1,16 @@
+//
+// Created by fuyangyang on 2018/7/9.
+//
+#include <stdio.h>
+#include <pthread.h>
+#include <iostream>
+using namespace std;
+
+void sayHello() {
+    cout << "hello world" << endl;
+}
+
+int main() {
+    cout << "hello world" << endl;
+}
+
